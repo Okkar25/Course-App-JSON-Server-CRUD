032955d361595781256e7615dcb622f7dec93382
Okkar25 <okkaaung2508@gmail.com>
2024-01-19 00:32:11 +0630

    second commit

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
 export const Config = {
-  api_url: "http://localhost:5173/api/courses",
-  // api_url: "https://course-app-json-crud.netlify.app/api/courses",
+  // api_url: "http://localhost:5173/api/courses",
+  api_url: "https://course-app-json-crud.netlify.app/api/courses",
 };
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import jsonServer from "vite-plugin-simple-json-server";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), jsonServer({ delay: 2000 })],
+  plugins: [react(), jsonServer({ delay: 1500 })],
 });
 
 
